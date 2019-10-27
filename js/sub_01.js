8cb122ff1de4c09177613535d93be31704bf4326
unknown <nayoungd02@naver.com>
2019-10-28 08:48:43 +0900

    second modify

--- js/sub_01.js
+++ js/sub_01.js
@@ -12,3 +12,21 @@ $(function () {
             'top': 200
         }, 700, null, baloonUp);
     }});
+
+        $(function(){
+            $('.sub01_sub02').on('click', function(){
+                alert('작업중인 페이지입니다.')
+            });
+        });
+
+        $(function(){
+            $('.sub01_sub03').on('click', function(){
+                alert('작업중인 페이지입니다.')
+            });
+        });
+
+        $(function(){
+            $('.sub01_sub04').on('click', function(){
+                alert('작업중인 페이지입니다.')
+            });
+        });
